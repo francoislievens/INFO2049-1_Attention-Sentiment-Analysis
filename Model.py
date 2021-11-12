4fc85640d0118059887a521276a2a2cb0ac3f1d0
François Lievens <francoislievens@outlook.com>
2021-11-12 23:41:59 +0100

    let's go

--- Model.py
+++ Model.py
@@ -11,12 +11,15 @@ from DatasetPreparator import prepare_csv
 from torchtext.legacy.data import Field, TabularDataset, BucketIterator
 
 DATA_PATH = 'data/aclImdb/'
+MODEL_PATH = 'model'
 TRAIN_SPLIT = 0.8
 EMBEDDING = 'glove'         # glove or fasttext
-BATCH_SIZE = 1
+BATCH_SIZE = 10
 DEVICE = 'cuda'
 NB_EPOCH = 10
 HIDDEN_SIZE = 1024
+LEARNING_RATE = 1e-4
+MODEL_NAME = 'Glove_Test'
 
 class SentimentModel(torch.nn.Module):
 
@@ -29,7 +32,7 @@ class SentimentModel(torch.nn.Module):
         self.name = name
         self.embed_size = embed_size
         self.device = device
-        self.hidden_size = 2000
+        self.hidden_size = HIDDEN_SIZE
         self.nb_heads = 4  # Number of heads for multi head self attention
 
         # The embedding layer
@@ -53,16 +56,17 @@ class SentimentModel(torch.nn.Module):
         self.output_sm = torch.nn.Sigmoid()
 
     def forward(self, x):
-        # Get shapess
-        N = x.size()[0]  # Batch size
-        lng = x.size()[1]  # length of sequences
+
         # Get the embedding of inputs
         # embed_x = self.embed_layer.forward(x, get_embed=True)
-        with torch.no_grad():
-            embed_x = self.embed_layer(x)
+        embed_x = self.embed_layer(x)
+        # Get batch on the first dim
+        embed_x = embed_x.permute(1, 0, 2)
 
-        print(embed_x.shape)
-        sys.exit(0)
+        # Get shapess
+        N = embed_x.size()[0]  # Batch size
+        lng = embed_x.size()[1]  # length of sequences
+
         # ==================================== #
         #             Encoder part             #
         # ==================================== #
@@ -78,8 +82,13 @@ class SentimentModel(torch.nn.Module):
         # ==================================== #
 
         # concatenate the two final states (since bi-directional lstm)
+        if True:
+            print('Final states: {}'.format(final_h_states.shape))
+            print('hidden states: {}'.format(hid_states.shape))
         final_state = torch.cat((final_h_states[0:1, :, :], final_h_states[1:2, :, :]),
-                                dim=2).reshape(N, -1, self.hidden_size * 2)
+                                dim=2)
+        print('concat shape: {}'.format(final_state.shape))
+        final_state = final_state.reshape(N, -1, self.hidden_size * 2)
         # Repeat this sequence to math the sequence length
         final_state = final_state.repeat(1, lng, 1)
         # concatenate with all hidden states
@@ -102,6 +111,34 @@ class SentimentModel(torch.nn.Module):
         return output
 
 
+def load_model(model, optimizer, model_path, model_name):
+    epoch_idx = 0
+    if not os.path.exists('{}/{}'.format(model_path, model_name)):
+        os.mkdir('{}/{}'.format(model_path, model_name))
+        file = open('{}/{}/train_logs.csv'.format(model_path, model_name), 'w')
+        file.write('Epoch,train_loss\n')
+        file.close()
+        file = open('{}/{}/test_logs.csv'.format(model_path, model_name), 'w')
+        file.write('Epoch,train_loss\n')
+        file.close()
+    else:
+        # Try to load model's weights
+        try:
+            model.load_state_dict((torch.load('{}/{}/model_weights.pt'.format(model_path, model_name))))
+            print('Previous model loaded')
+        except:
+            print('Impossible to load existing model: Maybe an existing empty folder exists')
+            sys.exit(1)
+        # Try to load optimizer weights
+        try:
+            optimizer.load_state_dict(torch.load('{}/{}/optimizer_weights.pt'.format(model_path, model_name)))
+            print('Optimizer weights loaded')
+        except:
+            print('Fail to load optimizer weights')
+        # Load epoch index if the model exists
+        train_logs = pd.read_csv('{}/{}/train_logs.csv'.format(model_path, model_name), sep=',')
+        epoch_idx = int(train_logs.iloc[-1]['Epoch']) + 1
+    return epoch_idx
 
 def train():
     # If csv file not already prepared:
@@ -144,28 +181,99 @@ def train():
     # Instanciate the model
     model = SentimentModel(input_size=input_size,
                            embed_size=300,
-                           name='Test',
+                           name=MODEL_NAME,
                            device=DEVICE).to(DEVICE)
     # Load pre-trained embedding parameters
     model.embed_layer.weight.data.copy_(text_field.vocab.vectors).to(DEVICE)
 
+    # Optimizer and loss function
+    optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
+    loss_fn = torch.nn.MSELoss()
 
+    # Tensorboard
+    tb = SummaryWriter()
 
-    start_epoch = 0
+    # Load exiting model
+    start_epoch = load_model(model=model,
+                             optimizer=optimizer,
+                             model_path=MODEL_PATH,
+                             model_name=MODEL_NAME)
 
     # Epoch loop
     for i in range(start_epoch, NB_EPOCH):
-        for step, batch in enumerate(train_iterator):
+
+        print('Training epoch {} / {}'.format(i, NB_EPOCH))
+        loop = tqdm(train_iterator, leave=True)
+
+        train_loss = []
+        for step, batch in enumerate(loop):
+            # Get data
             text = batch.t.to(DEVICE)
             sentiment = batch.s.to(DEVICE)
 
+            # Makes predictions
             pred = model(text)
 
-            print(pred)
-            print(text.shape)
-            print(sentiment)
-            sys.exit(0)
+            # Compute loss
+            loss = loss_fn(pred, sentiment)
 
+            # Backprop
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            # Logs
+            tb.add_scalar('{}_Train'.format(MODEL_NAME), loss.item(), i)
+            train_loss.append(loss.item())
+            loop.set_postfix(loss=loss.item)
+
+            if step % 500 == 0:
+                torch.save(model.state_dict(), '{}/{}/model_weights.pt'.format(MODEL_PATH, MODEL_NAME))
+                torch.save(optimizer.state_dict(), '{}/{}/optimizer_weights.pt'.format(MODEL_PATH, MODEL_NAME))
+                f = open('{}/{}/train_logs.csv'.format(MODEL_PATH, MODEL_NAME))
+                for itm in train_loss:
+                    f.write('{},{}\n'.format(i, itm))
+                f.close()
+                train_loss = []
+
+        torch.save(model.state_dict(), '{}/{}/model_weights.pt'.format(MODEL_PATH, MODEL_NAME))
+        torch.save(optimizer.state_dict(), '{}/{}/optimizer_weights.pt'.format(MODEL_PATH, MODEL_NAME))
+        for itm in train_loss:
+            f.write('{},{}\n'.format(i, itm))
+        f.close()
+
+        # Test loop
+        print('Testing epoch {} / {}'.format(i, NB_EPOCH))
+        loop = tqdm(test_iterator, leave=True)
+        test_loss = []
+        for step, batch in enumerate(loop):
+
+            with torch.no_grad():
+                # Get data
+                text = batch.t.to(DEVICE)
+                sentiment = batch.s.to(DEVICE)
+
+                # Makes predictions
+                pred = model(text)
+
+                # Compute loss
+                loss = loss_fn(pred, sentiment)
+
+            # Logs
+            tb.add_scalar('{}_Test'.format(MODEL_NAME), loss.item(), i)
+            test_loss.append(loss.item())
+            loop.set_postfix(loss=loss.item)
+
+            if step % 500 == 0:
+                f = open('{}/{}/test_logs.csv'.format(MODEL_PATH, MODEL_NAME))
+                for itm in test_loss:
+                    f.write('{},{}\n'.format(i, itm))
+                f.close()
+                test_loss = []
+        f = open('{}/{}/test_logs.csv'.format(MODEL_PATH, MODEL_NAME))
+        for itm in test_loss:
+            f.write('{},{}\n'.format(i, itm))
+        f.close()
 
 if __name__ == '__main__':
 
